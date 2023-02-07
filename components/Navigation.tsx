e87baa7e9a58f5e6799071a0d500f3f4887ee716
michael heller <michaelheller007@googlemail.com>
2023-02-07 23:07:29 +0100

    add filter

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Navigation.module.css';
 
 export default function Navigation() {
     return (
+        <>
         <nav className={styles.nav}>
             <div className={styles.title}><b>Songbook</b></div>
             <ul className={styles.list}>
@@ -18,6 +19,11 @@ export default function Navigation() {
                     </Link>
                 </li>
             </ul>
+            <div><input className={styles.filter} type='button' value={'Jazz'}/></div>
+            <div><input className={styles.filter} type='button' value={'Fingerpicking'}/></div>
+            <div><input className={styles.filter} type='button' value={'Blues'}/></div>
+            <div><input className={styles.filter} type='button' value={'Rock/Pop'}/></div>
         </nav>
+        </>
     );
 }
