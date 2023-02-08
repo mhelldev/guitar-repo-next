670bda61b152e2bf0ee3709a9123473133310d1b
michael heller <michaelheller007@googlemail.com>
2023-02-08 20:40:57 +0100

    style filter

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,28 +4,28 @@ import styles from '../styles/Navigation.module.css';
 
 export default function Navigation() {
     return (
-        <>
-        <nav className={styles.nav}>
-            <div className={styles.title}><b>Songbook</b></div>
-            <ul className={styles.list}>
-                <li className={styles.item}>
-                    <Link href="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/add-post">
-                        Add post
-                    </Link>
-                </li>
-            </ul>
+        <div className={styles.navgroup}>
+            <nav className={styles.nav}>
+                <div className={styles.title}><b>Songbook</b></div>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <Link href="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/add-post">
+                            Add post
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             <div className={styles.filtergroup}>
                 <div><input className={styles.filter} type='button' value={'Jazz'}/></div>
                 <div><input className={styles.filter} type='button' value={'Fingerpicking'}/></div>
                 <div><input className={styles.filter} type='button' value={'Blues'}/></div>
                 <div><input className={styles.filter} type='button' value={'Rock/Pop'}/></div>
             </div>
-        </nav>
-        </>
+        </div>
     );
 }
